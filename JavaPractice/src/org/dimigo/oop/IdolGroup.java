e6820ef606c95611231c4d7e1ac3e9a186ff2eab
user <user@DESKTOP-20PML9E>
2017-04-10 09:58:50 +0900

    IdolGroup

---
+++ JavaPractice/src/org/dimigo/oop/IdolGroup.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *	 |_ IdolGroup
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 10.
+ * </pre>
+ *
+ * @author user
+ * @version : 1.0
+ */
+public class IdolGroup {
+
+	public static void main(String[] args) {
+		String[] groupName = { "빅뱅", "2NE1", "걸스데이" };
+		String[][] memberName = { { "GD", "태양", "대성", "탑", "승리" }, { "CL", "산다라박", "박봄", "민지" },
+				{ "유라", "혜리", "소진", "민아" } };
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("<<" + groupName[i].toString() + ">>");
+			for (int j = 0; j < memberName[i].length; j++) {
+
+				System.out.println(memberName[i][j].toString());
+			}
+			System.out.println();
+		}
+
+	}
+
+}
