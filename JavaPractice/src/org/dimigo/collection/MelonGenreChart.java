0ff5a507bff7ee60214cb7bae6b3fc4a027e69b7
user <user@DESKTOP-20PML9E>
2017-06-12 19:29:33 +0900

    16차시

--- JavaPractice/src/org/dimigo/collection/MelonGenreChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonGenreChart.java
@@ -4,7 +4,9 @@
 package org.dimigo.collection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <pre>
@@ -12,39 +14,55 @@ import java.util.List;
  *	 |_ MelonGenreChart
  *
  * 1. 개요 : 
- * 2. 작성일 : 2017. 6. 5.
+ * 2. 작성일 : 2017. 6. 12.
  * </pre>
  *
  * @author user
  * @version : 1.0
  */
 public class MelonGenreChart {
-	public static class MelonChart {		   
-		public static void main(String[] args) {
-		      
-	     List<Music> list = new ArrayList<Music>();
-	     list.add(new Music("I LUV IT", "PSY"));
-		 list.add(new Music("맞지?", "언니쓰"));
-		      
-		 System.out.println("-- << 멜론 챠트 >> --"); printList(list);
-	     System.out.println("-- << 2위 곡 추가 >> --");
-		      
-	     list.add(1, new Music("SIGNAL", "트와이스"));
-	     printList(list);
-		      
-		      
-	     System.out.println("-- << 3위 곡 변경 >> --"); list.set(2, new Music("팔레트", "아이유")); printList(list);
-	     System.out.println("-- << 2위 곡 삭제 >> --"); list.remove(1); printList(list);
-	     System.out.println("-- << 전체 리스트 삭제 >> --"); list.clear(); printList(list);
-		    	      		      
-		}
-	     public static void printList(List<Music> list) {
-		      
-	      int cnt = 0;
-		      
-	      for(Music music: list)
-	    	  System.out.println( (++cnt) + ". " + music);   System.out.println("");   
-		      
-      	}
+	public static void main(String[] args) {
+
+		System.out.println("--<<멜론 장르별 차트>>--");
+
+		Map<String, List<Music>> map = new HashMap<>();
+		List<Music> list1 = new ArrayList<>();
+		list1.add(new Music("팔레트", "아이유"));
+		List<Music> list2 = new ArrayList<>();
+		list2.add(new Music("I LUV IT", "PSY"));
+		list2.add(new Music("맞지?", "언니쓰"));
+		map.put("발라드", list1);
+		map.put("댄스", list2);
+		printMap(map);
+		System.out.println();
+
+		System.out.println("--<<댄스 2위 곡 변경>>--");
+		list2.set(1, new Music("SIGNAL", "트와이스"));
+		map.put("댄스", list2);
+		printMap(map);
+
+		System.out.println();
+		System.out.println("--<<댄스 1위 곡 삭제>>--");
+		map.remove(list2.remove(0));
+		printMap(map);
+
+		System.out.println();
+		System.out.println("--<<전체 리스트 삭제>>--");
+		map.clear();
+		printMap(map);
+
 	}
+
+	public static void printMap(Map<String, List<Music>> map) {
+		for (String key : map.keySet()) {
+			System.out.println("[" + key + "]");
+			int index = 1;
+			for (Music a : map.get(key)) {
+				System.out.println(index + "." + a.toString());
+				index = index + 1;
+			}
+		}
+
+	}
+
 }
